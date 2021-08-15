6399ebdfa325764d70ade4e1931c5027b51a54d6
wuchuheng <wuchuheng@163.com>
2021-08-15 14:57:41 +0800

    init

---
+++ .vuepress/config.js
@@ -0,0 +1,113 @@
+module.exports = {
+  "title": "白日梦想家",
+  "description": "",
+  "dest": "public",
+  "head": [
+    [
+      "link",
+      {
+        "rel": "icon",
+        "href": "/favicon.ico"
+      }
+    ],
+    [
+      "meta",
+      {
+        "name": "viewport",
+        "content": "width=device-width,initial-scale=1,user-scalable=no"
+      }
+    ]
+  ],
+  "theme": "reco",
+  "themeConfig": {
+    "nav": [
+      {
+        "text": "Home",
+        "link": "/",
+        "icon": "reco-home"
+      },
+      {
+        "text": "TimeLine",
+        "link": "/timeline/",
+        "icon": "reco-date"
+      },
+      {
+        "text": "Docs",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "vuepress-reco",
+            "link": "/docs/theme-reco/"
+          }
+        ]
+      },
+      {
+        "text": "Contact",
+        "icon": "reco-message",
+        "items": [
+          {
+            "text": "GitHub",
+            "link": "https://github.com/recoluan",
+            "icon": "reco-github"
+          }
+        ]
+      }
+    ],
+    "sidebar": {
+      "/docs/theme-reco/": [
+        "",
+        "theme",
+        "plugin",
+        "api"
+      ]
+    },
+    "type": "blog",
+    "blogConfig": {
+      "category": {
+        "location": 2,
+        "text": "Category"
+      },
+      "tag": {
+        "location": 3,
+        "text": "Tag"
+      }
+    },
+    "friendLink": [
+      {
+        "title": "午后南杂",
+        "desc": "Enjoy when you can, and endure when you must.",
+        "email": "1156743527@qq.com",
+        "link": "https://www.recoluan.com"
+      },
+      {
+        "title": "vuepress-theme-reco",
+        "desc": "A simple and beautiful vuepress Blog & Doc theme.",
+        "avatar": "https://vuepress-theme-reco.recoluan.com/icon_vuepress_reco.png",
+        "link": "https://vuepress-theme-reco.recoluan.com"
+      }
+    ],
+    "logo": "/logo.png",
+    "search": true,
+    "searchMaxSuggestions": 10,
+    "lastUpdated": "Last Updated",
+    "author": "吴楚衡",
+    "authorAvatar": "/avatar.png",
+    "record": "xxxx",
+    "startYear": "2017",
+  },
+  plugins: [['@vuepress-reco/comments', {
+    solution: 'vssue',
+    options: {
+      title: '白日梦想家.留言',
+      platform: 'github',
+      owner: 'wuchuheng',
+      repo: 'https://github.com/wuchuheng/notebook',
+      clientId: '916449ad6806e1a2073b',
+      clientSecret: '8e3fb945f1aaf9fb09ee68140108e72b5f69fbfd',
+    }
+  }]],
+
+  "markdown": {
+    "lineNumbers": true
+  }
+}
